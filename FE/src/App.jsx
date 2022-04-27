884495d9c30b6b82a9a94997434262e967d03001
yunho <kimyouknow@naver.com>
2022-04-27 12:08:20 +0900

    Chore: 메인 레이아웃
    헤더 mainWidth설정

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import styled from "styled-components";
-import Main from "components/Main";
+import Main from "Pages/Main";
 
-const Header = styled.header`
+const HeaderContainer = styled.header`
   width: 100vw;
   height: 300px;
   border-bottom: 1px solid black;
   margin-bottom: 30px;
 `;
 
+const Header = styled.header`
+  width: ${({ theme: { layout } }) => layout.mainWidth};
+  height: 100%;
+  margin: 0 auto;
+`;
+
 function App() {
   return (
     <>
-      <Header>네비게이션바</Header>
+      <HeaderContainer>
+        <Header>네비게이션</Header>
+      </HeaderContainer>
       <Main />
       <div className="Modal"></div>
     </>
