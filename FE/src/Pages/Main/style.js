5a6c0bcfe93a3e140700458b9a64f812cc689962
sju02048 <sju02048@naver.com>
2022-04-28 17:39:26 +0900

    Design: 버튼 가운데 정렬

--- FE/src/Pages/Main/style.js
+++ FE/src/Pages/Main/style.js
@@ -4,6 +4,9 @@ export const Container = styled.main`
   width: ${({ theme: { layout } }) => layout.mainWidth};
   padding-top: 140px;
   margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export const MoreBtn = styled.button`
